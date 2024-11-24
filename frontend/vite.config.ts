5924bbaed731a1c459d6ef8237cec38bb70a5dad
Muhammed Ajmal k <ajmalchundappuram@gmail.com>
2024-11-24 15:09:06 +0530

    vercel.json added

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/", // added to the base
   server: {
     port: 3000,
   },
