7ae2d24e286bbf05574e0a927c7245aeb4be61d8
Muhammed Ajmal k <ajmalchundappuram@gmail.com>
2024-12-06 16:34:41 +0530

    image compressed v2

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,5 @@
-const { nextui } = require("@nextui-org/react");
-const withMT = require("@material-tailwind/react/utils/withMT");
+import { nextui } from "@nextui-org/react";
+import withMT from "@material-tailwind/react/utils/withMT";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = withMT({
